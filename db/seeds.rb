54c626657e0e974f1ad4ed3be72a640124a26e6d
Mirko Akov <mupkoo@gmail.com>
2015-07-27 15:51:55 +0300

    Add some dummy seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'fabrication'
+require 'faker'
+
+Fabricator(:author) do
+    name { Faker::Name.name }
+end
+
+Fabricator(:post) do
+    author { Fabricate(:author) }
+    title  { Faker::Lorem.sentence }
+    body   { Faker::Lorem.paragraphs([3,5,7,9].sample).map { |p| "<p>#{p}</p>" }.join('') }
+end
+
+Fabricator(:comment) do
+    post { Fabricate(:post) }
+    body { Faker::Lorem.paragraph([1,2,3].sample) }
+end
+
+Fabricate.times(3, :author).each do |author|
+    Fabricate.times([10,15,20].sample, :post, author: author).each do |post|
+        Fabricate.times([0,1,3,4,5].sample, :comment, post: post)
+    end
+end

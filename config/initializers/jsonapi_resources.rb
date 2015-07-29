1e0d42fff82d7e1c9dadb8f3b4028cc5f570e8c5
Mirko Akov <mupkoo@gmail.com>
2015-07-29 10:37:52 +0300

    Add pagination to the API responses

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -1,3 +1,7 @@
 JSONAPI.configure do |config|
     config.json_key_format = :camelized_key
+
+    config.default_paginator = :paged
+    config.default_page_size = 10
+    config.maximum_page_size = 50
 end

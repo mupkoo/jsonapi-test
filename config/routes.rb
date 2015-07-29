79cd36a88c578e1b3624250709e097d69d22fda1
Mirko Akov <mupkoo@gmail.com>
2015-07-29 17:13:38 +0300

    Uncomment back the base api routes :grin:

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-    # namespace :api do
-    #     namespace :base do
-    #         resources :authors,  only: [:index, :show, :create, :update, :destroy]
-    #         resources :posts,    only: [:index, :show, :create, :update, :destroy]
-    #         resources :comments, only: [:index, :show, :create, :update, :destroy]
-    #     end
-    # end
+    namespace :api do
+        namespace :base do
+            resources :authors,  only: [:index, :show, :create, :update, :destroy]
+            resources :posts,    only: [:index, :show, :create, :update, :destroy]
+            resources :comments, only: [:index, :show, :create, :update, :destroy]
+        end
+    end
 
 
     namespace :api do
